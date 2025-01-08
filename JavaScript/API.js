d1e6e0b408fc0872eea7242845cbf7c70c236f0a
sharet9446 <spees97@naver.com>
2025-01-08 19:06:21 +0900

    파일생성

---
+++ JavaScript/API.js
@@ -0,0 +1,2 @@
+const apiKey = "de05a24ba0e949865e50fb96c539de0d"
+const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # NBC_Movie_Review
+영화 리뷰하는 사이트 입니다.
