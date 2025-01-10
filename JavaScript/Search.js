61550828bad0c99d7eba9cd6e109cae6d0c0a1a4
sharet9446 <spees97@naver.com>
2025-01-10 14:32:55 +0900

    검색 기능 공백 제거, 소문자로 통일된 검색

--- JavaScript/Search.js
+++ JavaScript/Search.js
@@ -1,14 +1,19 @@
 // 검색기능
 document.querySelector('#searchForm').addEventListener('keyup', (event) => {
 
-        const searcheName = event.target.value;
-        const movieTitle = document.querySelectorAll('.movieTitle');
-        movieTitle.forEach(title => {
-            if (title.innerText.includes(searcheName)) {
-                title.parentElement.parentElement.style.display = 'block';
-            } else {
-                title.parentElement.parentElement.style.display = 'none';
-            }
-        })
+    const searchName = event.target.value.replace(/\s/g, "").toLowerCase();
+    const movieTitle = document.querySelectorAll('.movieTitle');
+
+    movieTitle.forEach(title => {
+        
+        let trimTitle = title.innerText.replace(/[\s=:/()]/g, "").toLowerCase();
+        let movieCard = title.closest('.movieCard');
+
+        if (trimTitle.includes(searchName)) {
+            movieCard.style.display = 'block';
+        } else {
+            movieCard.style.display = 'none';
+        }
+    })
 
 })
